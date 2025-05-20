f429b0fca2805d1821e1565511b9bae0f62f543a
Darni Nishanth <90642710+DarniNishanth@users.noreply.github.com>
2025-05-20 11:27:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
-from datetime import date
+from datetime import datetime
 
 response  = requests.get("https://timesofindia.indiatimes.com/?from=mdr")
 soup = BeautifulSoup(response.text,'html.parser')
 temp = soup.select('.col_l_6')
 with open('data.text', 'a') as file:
-    today = date.today()
-    file.write(today+'\n')
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    file.write(now_str+'\n')
     for i in temp:
         if i.find('img') is not None:
             file.write(i.find('figcaption').text+'\n')
