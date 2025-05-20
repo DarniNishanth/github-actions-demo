c74f64d68377a00cb34084ec35dd26407063a0c5
Darni Nishanth <90642710+DarniNishanth@users.noreply.github.com>
2025-05-20 11:23:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import date
 
-response  = requests.get("https://timesofindia.indiatimes.com/india")
-soup = BeautifulSoup(response.text,'html')
-temp = soup.selector('.WavNE ')
+response  = requests.get("https://timesofindia.indiatimes.com/?from=mdr")
+soup = BeautifulSoup(response.text,'html.parser')
+temp = soup.select('.col_l_6')
 with open(filename, 'a') as file:
     today = date.today()
     file.write(today+'\n')
-    for i in temp.contents:
-        file.write(i+'\n')
-        file.write(data + '\n')
+    for i in temp:
+        if i.find('img') is not None:
+            file.write(i.find('figcaption').text+'\n')
