1fd8626761f436bc1ea2d947865d193de823de19
Darni Nishanth <90642710+DarniNishanth@users.noreply.github.com>
2025-05-20 11:07:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import date
 
 response  = requests.get("https://timesofindia.indiatimes.com/india")
 soup = BeautifulSoup(response.text,'html')
 temp = soup.selector('.WavNE ')
-for i in temp.contents:
-    print(i)
+with open(filename, 'a') as file:
+    today = date.today()
+    file.write(today+'\n')
+    for i in temp.contents:
+        file.write(i+'\n')
+        file.write(data + '\n')
