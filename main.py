b25fc1dbf3d023b4a5c0913b57d4f7efb4a17f23
Darni Nishanth <90642710+DarniNishanth@users.noreply.github.com>
2025-05-20 11:24:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import date
 response  = requests.get("https://timesofindia.indiatimes.com/?from=mdr")
 soup = BeautifulSoup(response.text,'html.parser')
 temp = soup.select('.col_l_6')
-with open(filename, 'a') as file:
+with open('data.text', 'a') as file:
     today = date.today()
     file.write(today+'\n')
     for i in temp:
